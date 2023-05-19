efa183688c86a3fd631400f4dadd5a0c5fd4806f
Soumya-9641 <98831819+Soumya-9641@users.noreply.github.com>
2023-05-19 21:57:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (82.71%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int cnt=0;
+    bool divisorGame(int n) {
+        
+      if(n%2==0)
+          return true;
+          
+        
+    return false;
+        
+        
+    }
+};
