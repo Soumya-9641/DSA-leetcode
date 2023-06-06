c959b39cad2e47a7af0f60a0c69263b5e8ad6c20
Soumya-9641 <98831819+Soumya-9641@users.noreply.github.com>
2023-06-06 12:10:47 +0530

    Time: 156 ms (34.51%), Space: 57.7 MB (43.22%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+       bool isPalindrome(string s,int start,int end){
+        while(start<=end){
+            if(s[start++]!=s[end--])
+                return false;
+        }
+        return true;
+    }
+    
+    void findPath(int idx,string s,vector<string>&path, vector<vector<string>>&res){
+        if(idx==s.size()){
+            res.push_back(path);
+            return ;
+        }   
+            for(int i=idx;i<s.size();++i){
+                if(isPalindrome(s,idx,i)){
+                    path.push_back(s.substr(idx,i-idx+1));
+                    findPath(i+1,s,path,res);
+                    path.pop_back();
+                }
+            }
+        }
+    
+    
+ 
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>res;
+        vector<string>path;
+        findPath(0,s,path,res);
+        return res;
+    }
+};
