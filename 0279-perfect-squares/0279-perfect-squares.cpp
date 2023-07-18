42f17987e66cf39519fe9f35f9ebb7bb37ad0e7c
Soumyadip <98831819+Soumya-9641@users.noreply.github.com>
2023-07-18 10:25:10 +0530

    Time: 179 ms (78.50%), Space: 8.9 MB (85.60%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        int cnt=1;
+        while(cnt*cnt<=n){
+            int sq= cnt*cnt;
+            for(int i = sq;i<n+1;i++){
+                dp[i]=min(dp[i-sq]+1,dp[i]);
+                
+            }
+            cnt++;
+        }
+        return dp[n];
+    }
+};
