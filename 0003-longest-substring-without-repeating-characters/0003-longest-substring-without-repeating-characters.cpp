d204f670ed88cdee0b27269011e82facf70a3749
Soumyadip <98831819+Soumya-9641@users.noreply.github.com>
2023-07-27 12:37:24 +0530

    Time: 37 ms (29.36%), Space: 10.9 MB (23.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.length()==0)
-            return 0;
-        int i=0,j=0;
-        unordered_map<char,int>mp;
+//         if(s.length()==0)
+//             return 0;
+//         int i=0,j=0;
+//         unordered_map<char,int>mp;
         
-        int ans =INT_MIN;
-        while(j<s.length()){
-            mp[s[j]]++;
-            if(mp.size()==j-i+1){
-                ans=max(ans,j-i+1);
+//         int ans =INT_MIN;
+//         while(j<s.length()){
+//             mp[s[j]]++;
+//             if(mp.size()==j-i+1){
+//                 ans=max(ans,j-i+1);
+//             }
+//             else if(mp.size()<j-i+1){
+//                 while(mp.size()<j-i+1){
+//                     mp[s[i]]--;
+//                     if(mp[s[i]]==0){
+//                         mp.erase(s[i]);
+//                     }
+//                     i++;
+//                 }
+//             }
+//             j++;
+//         }
+//         return ans;
+        
+        //using unordered_set
+        int n = s.size();
+        if(s.length()==0)
+          return 0;
+        int i=0,j=0;
+        int ans=INT_MIN;
+        unordered_set<char>set;
+        while(j<n){
+            if(set.find(s[j])==set.end()){
+                set.insert(s[j]);
+                ans = max(ans,j-i+1);
+                j++;
             }
-            else if(mp.size()<j-i+1){
-                while(mp.size()<j-i+1){
-                    mp[s[i]]--;
-                    if(mp[s[i]]==0){
-                        mp.erase(s[i]);
-                    }
-                    i++;
-                }
+            else{
+                set.erase(s[i]);
+                i++;
             }
-            j++;
         }
         return ans;
     }
